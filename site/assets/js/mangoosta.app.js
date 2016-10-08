d301f92850e88b21b396d968b6e88700c178bd18
Jhony Grillet <grilletjhony@gmail.com>
2016-10-07 21:12:08 -0400

    Modified responsive design, content & router

---
+++ site/assets/js/mangoosta.app.js
@@ -0,0 +1,50 @@
+var $ = require('minified').$,
+		$$ = require('minified').$$,
+		routes = ["inicio", "empresa", "tecnologias", "productos", "contacto", "idioma"],
+		actualPage = routes[0];
+
+function resizeAndShowMenu() {
+	var viewport = $(screen).get("width");
+	if (viewport <= 960) {
+		$(".mangoosta-items").hide();
+	} else {
+		$(".mangoosta-items").show();
+	}
+}
+
+function getPage(indexRoute) {
+	actualPage = routes[indexRoute];
+	$("section").hide();
+	$("#mangoosta-content-"+actualPage).show();
+}
+
+function isOtherPage(indexRoute) {
+	return routes[indexRoute] !== actualPage;
+}
+
+$(function () {
+
+	var toggleResponsiveMenu = $(".mangoosta-items").toggle(
+		{"$$show": 0},
+		{"$$show": 1}
+	);
+
+	resizeAndShowMenu();
+	getPage(0);
+
+	$(window).on("resize", resizeAndShowMenu);
+
+	$(".mangoosta-menu").onClick(function (event) {
+		$(event.target).set("")
+	});
+
+	$(".menu").onClick(function (event, index) {
+		$(".menu").up().set("-mangoosta-secondary-back");
+		$(event.target).up().set("+mangoosta-secondary-back");
+		if (isOtherPage(index)) {
+			getPage(index);
+		}
+	});
+
+
+})
