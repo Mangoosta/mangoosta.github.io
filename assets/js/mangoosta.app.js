f0380385b19786c86acbeeda022e41d51d67c3c3
Jhony Alberto Grillet Medina <ch1nux@dbadminubv>
2016-11-16 17:46:11 -0400

    New Assets for Mangoosta site

---
+++ assets/js/mangoosta.app.js
@@ -0,0 +1,79 @@
+var $ = require('minified').$,
+		$$ = require('minified').$$,
+		HTML = require('minified').HTML,
+		menu = $(".mangoosta-item").sub(1),
+		actualPage = "#mangoosta-content-inicio",
+		tplRepo = '{{each}}<div class="mangoosta-column-6 mangoosta-responsive-column"><div class="mangoosta-card"><div class="mangoosta-grid"><div class="mangoosta-column-4 mangoosta-centered-text"><img src="assets/img/logos/GitHub-Mark.png" height="108" width="108" alt=""></div><a href="{{this.url}}" target="_blank"><h3>{{this.nombre}}</h3></a><p>Hecho en: {{this.lenguaje}}</p><p>{{this.descripcion}}</p></div></div></div>{{/each}}';
+
+
+function showMenu() {
+	var viewport = $(window).get("innerWidth");
+	toggleShowMenu((viewport <= 960));
+}
+
+function toggleShowMenu(show) {
+	if (show) {
+		$(".mangoosta-item").only(0).show();
+		menu.map(function (element) {
+			return $(element).hide();
+		});
+	} else {
+		$(".mangoosta-item").only(0).hide();
+		menu.map(function (element) {
+			return $(element).show();
+		});
+	}
+}
+
+function makeGithubRequest() {
+	$.request('GET', "https://api.github.com/users/Mangoosta/repos").then(function(text) {
+			var repos = $.parseJSON(text);
+			var repoList = repos.map(function (repo) {
+				return {
+					nombre: repo.name,
+					url: repo.html_url,
+					descripcion: repo.description,
+					lenguaje: repo.language
+				};
+			});
+			$("#repos").ht(tplRepo, repoList);
+		}
+	)
+}
+
+function getPage(idPage) {
+	$("section").hide();
+	$(idPage).show();
+}
+
+function setActualPage(idPage) {
+	actualPage = idPage;
+}
+
+function isOtherPage(idPage) {
+	return idPage !== actualPage;
+}
+
+$(function () {
+
+	showMenu();
+
+	makeGithubRequest();
+
+	getPage(actualPage);
+
+	$(window).on("resize", showMenu);
+
+	$("#menu-active").onClick(function () {
+		console.info("hizo click!");
+	});
+
+	$(".mangoosta-item a").onClick(function (event) {
+		var href = event.target.href.split("#")[1];
+		if (isOtherPage("#"+href)) {
+			getPage("#"+href);
+			setActualPage("#"+href);
+		}
+	});
+
+});
