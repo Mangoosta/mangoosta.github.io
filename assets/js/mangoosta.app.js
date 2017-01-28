66f2b004914347ee52cd7c6745077018da0fd514
Jhony Grillet <grilletjhony@gmail.com>
2017-01-28 10:54:30 -0400

    [Feature] Add fully functional contact form. Powered by JEXIA

--- assets/js/mangoosta.app.js
+++ assets/js/mangoosta.app.js
@@ -6,6 +6,13 @@ var $ = require('minified').$,
 		tglMenu = menu.toggle({"$$show": 0}, {"$$show": 1}, 200),
 		tplRepo = '{{each}}<div class="mangoosta-column-6 mangoosta-responsive-column"><div class="mangoosta-card mangoosta-smooth-anim"><div class="mangoosta-grid"><div class="mangoosta-column-4 mangoosta-centered-text"><img src="assets/img/logos/GitHub-Mark.png" height="108" width="108" alt="" class="mangoosta-hide-on-mobile"></div><a href="{{this.url}}" target="_blank"><h3>{{this.nombre}}</h3></a><p>Hecho en: {{this.lenguaje}}</p><p>{{this.descripcion}}</p></div></div></div>{{/each}}';
 
+var auth = {
+		key: '3da85a23027c9552b43d2c6466be9673',
+		secret: '8c354a2b143f598a3b8c3ffbf5f5b84ecf86d163d2923125'
+	},
+	baseUrl = 'https://df056d30-d245-11e6-ae5d-89b2a570b795.app.jexia.com';
+
+
 function setPage (idPage) {
 	$("section").hide();
 	$(idPage).show();
@@ -82,6 +89,19 @@ function makeGithubRequest() {
 	}
 }
 
+function makeContactRequest (information) {
+	$.request('POST', baseUrl, auth).then(function(text) {
+		var data = $.parseJSON(text),
+		settings = { headers: { 'Authorization': 'Bearer ' + data.token, 'Access-Control-Allow-Origin': '*' } };
+			return $.request('POST', baseUrl + '/comentarios', information, settings);
+		}).then(function () {
+			swal("Gracias por contactarnos", "Visítanos pronto", "success");
+		}).error(function () {
+			swal("Oh, Oh!", "Debiste haber escrito algo mal... :/", "error");
+		});
+	}
+}
+
 $(function () {
 
 	var router = new Navigo("/", true);
@@ -99,7 +119,7 @@ $(function () {
 	$("#send-commentary").onClick(function (event) {
 		event.preventDefault();
 		var form_data = $("input, textarea").values();
-		swal("Gracias por contactarnos", "Visítanos pronto", "success");
+		makeContactRequest(form_data);
 		$("input, textarea").set("value", "");
 	});
 });
